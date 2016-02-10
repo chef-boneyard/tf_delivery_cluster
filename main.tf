a04e0c472915a647606798d310665f9e2d776930
Salim Afiune <afiune@chef.io>
2016-02-10 16:55:48 -0500

    Use tf_chef_delivery_build_node Module

--- main.tf
+++ main.tf
@@ -37,27 +37,30 @@ module "chef-delivery" {
   security_groups_ids = "${module.ec2-network.chef-delivery_security_group_id}"
   key_name = "${var.aws_key_pair_name}"
   user = "${var.aws_ami_user}"
-  private_key_path = ".keys/${var.aws_key_pair_name}.pem"
+  private_key_path = "${path.cwd}/.keys/${var.aws_key_pair_name}.pem"
   chef-server-url = "${module.chef-server.chef-server-url}"
 
   enterprise = "${var.chef-delivery-enterprise}"
 }
 
 # Setup Chef Build-Node(s)
-module "chef-build-node" {
-  source = "./chef-build-node"
-  ami = "${lookup(var.centos-6-amis, var.aws_default_region)}"
-  count = "${lookup(var.instance_counts, "chef-build-node")}"
-  instance_type = "${lookup(var.instances, "chef-build-node")}"
-  subnet_id = "${module.ec2-network.subnet_id}"
-  security_groups_ids = "${module.ec2-network.chef-build-node_security_group_id}"
-  key_name = "${var.aws_key_pair_name}"
-  user = "${var.aws_ami_user}"
-  private_key_path = ".keys/${var.aws_key_pair_name}.pem"
-  chef-server-url = "${module.chef-server.chef-server-url}"
-  delivery_builder_keys = "${module.chef-delivery.delivery_builder_keys}"
+module "chef_delivery_build_node" {
+  source                  = "github.com/chef/tf_chef_delivery_build_node"
+  aws_ami_id              = "${lookup(var.centos-6-amis, var.aws_default_region)}"
+  aws_ami_user            = "${var.aws_ami_user}"
+  aws_flavor              = "${lookup(var.instances, "chef-build-node")}"
+  aws_subnet_id           = "${module.ec2-network.subnet_id}"
+  aws_security_groups_ids = "${module.ec2-network.chef-build-node_security_group_id}"
+  instance_count          = "${lookup(var.instance_counts, "chef-build-node")}"
+  aws_key_name            = "${var.aws_key_pair_name}"
+  aws_private_key_file    = "${path.cwd}/.keys/${var.aws_key_pair_name}.pem"
+  chef_server_url         = "${module.chef-server.chef-server-url}"
+  delivery_enterprise     = "terraform"
+  chef_organization       = "terraform"
+  chef_environment        = "_default"
   # Maybe be this will create a dependency
-  enterprise = "${module.chef-delivery.chef-delivery-enterprise}"
+  # delivery_builder_keys = "${module.chef-delivery.delivery_builder_keys}"
+  delivery_enterprise     = "${module.chef-delivery.chef-delivery-enterprise}"
 }
 
 # Setup Chef Supermarket
